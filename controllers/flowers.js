fc7fa2e54810a317df510f66bda73c78aa53761d
Niharica Myla <89415756+NiharicaMyla@users.noreply.github.com>
2021-11-09 23:31:00 -0600

    changes

--- controllers/flowers.js
+++ controllers/flowers.js
@@ -10,25 +10,7 @@ exports.flowers_detail = function (req, res) {
   res.send("NOT IMPLEMENTED: flowers detail: " + req.params.id);
 };
 
-// Handle flowers create on POST.
-exports.flowers_create_post = async function (req, res) {
-    console.log(req.body);
-    let document = new flowers();
-    // We are looking for a body, since POST does not have query parameters.
-    // Even though bodies can be in many different formats, we will be picky
-    // and require that it be a json object
-    // {"flower_type":"lobelias", "cost":35, "color":"blue"}
-    document.flower_type = req.body.flower_type;
-    document.duration = req.body.duration;
-    document.cost = req.body.cost;
-    try {
-      let result = await document.save();
-      res.send(result);
-    } catch (err) {
-      res.status(500);
-      res.send(`{"error": ${err}}`);
-    }
-  };
+
 // Handle flowers delete form on DELETE.
 exports.flowers_delete = function (req, res) {
   res.send("NOT IMPLEMENTED: flowers delete DELETE " + req.params.id);
