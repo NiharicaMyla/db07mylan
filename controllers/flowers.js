3da3a3c67ca6b0d1771ea59924b15da9f2b9df01
Niharica Myla <89415756+NiharicaMyla@users.noreply.github.com>
2021-11-15 21:00:21 -0600

    detail

--- controllers/flowers.js
+++ controllers/flowers.js
@@ -105,3 +105,17 @@ exports.flowers_view_all_Page = async function (req, res) {
     res.send(`{"error": ${err}}`);
   }
 };
+
+// Handle a show one view with id specified by query
+exports.flowers_view_one_Page = async function(req, res) {
+  console.log("single view for id " + req.query.id)
+  try{
+  result = await flowers.findById( req.query.id)
+  res.render('flowersdetail',
+  { title: 'flowers Detail', toShow: result });
+  }
+  catch(err){
+  res.status(500)
+  res.send(`{'error': '${err}'}`);
+  }
+  };
--- routes/flowers.js
+++ routes/flowers.js
@@ -15,11 +15,11 @@ router.get("/", flowers_controlers.flowers_view_all_Page);
 router.get("/detail", flowers_controlers.flowers_view_one_Page);
 
 /* GET create costume page */
-router.get("/create", flowers_controlers.flowers_create_Page);
+// router.get("/create", flowers_controlers.flowers_create_Page);
 
 /* GET create update page */
-router.get("/update", flowers_controlers.flowers_update_Page);
+// router.get("/update", flowers_controlers.flowers_update_Page);
 
 /* GET create costume page */
-router.get("/delete", flowers_controlers.flowers_delete_Page);
+// router.get("/delete", flowers_controlers.flowers_delete_Page);
 module.exports = router;
