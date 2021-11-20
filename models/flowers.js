a317d090c6cd3ccbd47ae9b6148761c74b2fcc3c
Niharica Myla <89415756+NiharicaMyla@users.noreply.github.com>
2021-11-19 22:09:37 -0600

    account created

--- models/flowers.js
+++ models/flowers.js
@@ -4,7 +4,11 @@ const { SchemaTypes } = require('mongoose');
 const flowersSchema = mongoose.Schema({
 flowers_type: String,
 color: String,
-cost: Number
+cost: Number,
+
+        min:[15,"Minimum year for construction"],
+        max:[60,"Maximum year for construction"]
+
 })
 
 module.exports = mongoose.model("flowers", flowersSchema);
