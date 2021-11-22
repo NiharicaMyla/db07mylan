589c3c85fb0284dbd22588d10f599cbd4c1411ca
Niharica Myla <89415756+NiharicaMyla@users.noreply.github.com>
2021-11-21 18:46:42 -0600

    btns

--- models/flowers.js
+++ models/flowers.js
@@ -3,6 +3,6 @@ const { SchemaTypes } = require('mongoose');
 
 const flowersSchema = mongoose.Schema({
 flowers_type: String,
-color: {type: String, enum: ['white', 'red'], required: function() { return this.color;}},
+color: {type: String, enum: ['White', 'Red','Yellow','Green', 'Blue'], required: function() { return this.color;}},
 cost: {type: Number, min:[10], max:[200] }})
 module.exports = mongoose.model("flowers", flowersSchema);
